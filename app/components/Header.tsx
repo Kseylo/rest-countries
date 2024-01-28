f5da4388c42df3496911550c0159b761573c1597
Kseylo <alekseysarychev29@gmail.com>
2024-01-28 08:12:16 +0700

    Added mounted state to Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,20 @@
 
 import { useTheme } from 'next-themes'
 import { IoMoonOutline, IoMoon } from 'react-icons/io5'
+import { useEffect, useState } from 'react'
 
 export function Header() {
+    const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
     return (
         <div
             className={`dark:bg-dark-blue flex items-center justify-between bg-white px-4 py-8 shadow-md`}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-'use client'
-
-import { useTheme } from 'next-themes'
-
 export default function Home() {
-    const { theme, setTheme } = useTheme()
-    return (
-        <main className={`min-h-dvh`}>
-            <button onClick={() => setTheme('light')}>light</button>
-            <button onClick={() => setTheme('dark')}>dark</button>
-        </main>
-    )
+    return <main className={`min-h-dvh`}></main>
 }
