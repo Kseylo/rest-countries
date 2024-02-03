94da946d369fd83d2dd81f3f27ff6e99f47c0791
Kseylo <alekseysarychev29@gmail.com>
2024-02-03 13:17:28 +0700

    Removed delayed getCoutries

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,4 +1,4 @@
-export { getCountries, getCountriesWithDelay } from './getCountries'
+export { getCountries } from './getCountries'
 export { getCountry } from './getCountry'
 export { getBordersCountries } from './getBorderCountries'
 export type { Country, CountryDetailed, BorderCountry } from './types'
